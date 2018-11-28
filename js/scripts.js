568028a42a0d0c83a7e4223e50b83daea98b1b20
Alex Ogola <ogollaokumu@gmail.com>
2018-11-28 14:30:38 +0300

    initial calculator development

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+var firstNumber = parseInt(prompt("Enter a number"));
+var secondNumber= parseInt(prompt("Enter a second number"));
+alert(add(firstNumber, secondNumber));
